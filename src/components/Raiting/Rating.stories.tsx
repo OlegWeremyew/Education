e31c6dcf0362d6cfb4d5ce19c6f9de947a87b2dc
OlegWeremyew <olegweremey1994@mail.ru>
2021-12-07 08:22:18 +0300

    deleted folder stories

--- src/components/Raiting/Rating.stories.tsx
+++ src/components/Raiting/Rating.stories.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import {Rating} from "./Rating";
 
@@ -8,9 +7,9 @@ export default {
     component: Rating,
 }
 
-export const EmptyRating = () => <Rating value={0} onClick={(x)=>{x}}/>
-export const Rating1 = () => <Rating value={1} onClick={(x)=>{x}}/>
-export const Rating2 = () => <Rating value={2} onClick={(x)=>{x}}/>
-export const Rating3 = () => <Rating value={3} onClick={(x)=>{x}}/>
-export const Rating4 = () => <Rating value={4} onClick={(x)=>{x}}/>
-export const Rating5 = () => <Rating value={5} onClick={(x)=>{x}}/>
+export const EmptyRating = () => <Rating value={0} onClick={x => x}/>
+export const Rating1 = () => <Rating value={1} onClick={x => x}/>
+export const Rating2 = () => <Rating value={2} onClick={x => x}/>
+export const Rating3 = () => <Rating value={3} onClick={x => x}/>
+export const Rating4 = () => <Rating value={4} onClick={x => x}/>
+export const Rating5 = () => <Rating value={5} onClick={x => x}/>
