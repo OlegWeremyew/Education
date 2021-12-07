b99cea5a2798d5f06e0fbb3c27b0c46527cf3b47
OlegWeremyew <olegweremey1994@mail.ru>
2021-12-07 08:31:35 +0300

    function RatingChanging

--- src/components/Raiting/Rating.stories.tsx
+++ src/components/Raiting/Rating.stories.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-import {Rating} from "./Rating";
+import {RaitingValueType, Rating,} from "./Rating";
 
 export default {
     title: 'Rating stories',
@@ -13,3 +13,7 @@ export const Rating2 = () => <Rating value={2} onClick={x => x}/>
 export const Rating3 = () => <Rating value={3} onClick={x => x}/>
 export const Rating4 = () => <Rating value={4} onClick={x => x}/>
 export const Rating5 = () => <Rating value={5} onClick={x => x}/>
+export const RatingChanging = () => {
+    const [rating, setRating] = useState<RaitingValueType>(3);
+    return <Rating value={rating} onClick={setRating}/>;
+}
