f864a6a2916a656777fcba2847476aa9b778f653
OlegWeremyew <olegweremey1994@mail.ru>
2021-12-07 12:16:06 +0300

    Complite Input.stories.tsx

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {ChangeEvent, useRef, useState} from 'react';
 import {action} from "@storybook/addon-actions";
 
 
@@ -11,18 +11,27 @@ export const UncontrolledInput = () => <input/>
 
 export const TrackValueOfUncontrolledInput = () => {
     const [value, setValue] = useState("")
-    return <><input onChange={(e) => {
+
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        debugger;
         const actialValue = e.currentTarget.value
-        setValue(actialValue) //можно сократить объеденив 2 строки
-    }}/> - {value}</>
+        setValue(actialValue)
+    }
+
+    return <><input onChange={onChange}/> - {value}</>
 }
 
 export const GetValueOfUncontrolledInputByButtonPress = () => {
     const [value, setValue] = useState("")
+    const inputRef = useRef<HTMLInputElement>(null)
 
-    return <><input/>
-        <button onClick={
-            () => {setValue("yo")}}>
+    const save = () => {
+        const el = inputRef.current as HTMLInputElement //Воспринимать как
+        setValue(el.value)
+    }
+
+    return <><input ref={inputRef}/>
+        <button onClick={save}>
             save
         </button>
         - actual value: {value}</>
