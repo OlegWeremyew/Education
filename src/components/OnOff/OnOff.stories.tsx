e345394271de2fa4f7e2507579749d60afdb67c8
OlegWeremyew <olegweremey1994@mail.ru>
2021-12-07 08:59:51 +0300

    OnOff complited

--- src/components/OnOff/OnOff.stories.tsx
+++ src/components/OnOff/OnOff.stories.tsx
@@ -7,8 +7,10 @@ export default {
     component: OnOff,
 }
 
-export const OnMode = () => <OnOff on={true} onChange={action('on or off clicked')}/>
-export const OffMode = () => <OnOff on={false} onChange={action('on or off clicked')}/>
+const callback = action('on or off clicked')
+
+export const OnMode = () => <OnOff on={true} onChange={callback}/>
+export const OffMode = () => <OnOff on={false} onChange={callback}/>
 
 export const ModeChanging = () => {
     const [value, setValue] = useState<boolean>(false);
