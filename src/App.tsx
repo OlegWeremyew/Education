bf0e6965843818820370feb2952759d093337239
olegweremyew <olegweremey1994@mail.ru>
2022-01-18 17:54:56 +0300

    6 learn useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import './App.css';
-import Accordion from "./components/Accordion";
+import Accordion from "./components/Accordion/Accordion";
 import {Rating} from "./components/Rating/Rating";
+import OnOff from "./components/OnOff/OnOff";
+import UncontrolledAccordion from "./components/UncontrolledAccordion/UncontrolledAccordion";
 
 const App = () => {
     return (
         <div>
+          {/*  <OnOff/>
+            <OnOff/>
+            <OnOff/>*/}
+
             <Accordion titleValue={"Menu"} collapsed={true}/>
             <Accordion titleValue={"Users"} collapsed={false}/>
 
-            <Rating value={1}/>
+            <UncontrolledAccordion titleValue={"Menu"}/>
+            <UncontrolledAccordion titleValue={"Users"}/>
+
+         {/*   <Rating value={1}/>
             <Rating value={2}/>
             <Rating value={3}/>
             <Rating value={4}/>
-            <Rating value={5}/>
+            <Rating value={5}/>*/}
         </div>
     )
 }
---
+++ src/components/UncontrolledAccordion/UncontrolledAccordion.tsx
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+
+type AccordionPropsType = {
+    titleValue: string
+}
+
+type AccordionTitlePropsType = {
+    title: string
+    setCollapsed: (collapsed: boolean) => void
+    collapsed: boolean
+}
+
+const UncontrolledAccordion = (props: AccordionPropsType) => {
+
+    let [collapsed, setCollapsed] = useState<boolean>(false)
+
+    return (
+        <div>
+            <AccordionTitle title={props.titleValue} setCollapsed={setCollapsed} collapsed={collapsed}/>
+            {collapsed && <AccordionBody/>}
+        </div>
+    )
+}
+
+function AccordionTitle(props: AccordionTitlePropsType) {
+    return (
+        <div onClick={() => {
+            props.setCollapsed(!props.collapsed)
+        }}>
+            <h3>{props.title}</h3>
+        </div>
+    )
+}
+
+function AccordionBody() {
+    return (
+        <div>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
+
+export default UncontrolledAccordion
