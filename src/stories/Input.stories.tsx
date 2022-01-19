e888a6c71a7598e2128db4b71f94b22ac5b7a618
olegweremyew <olegweremey1994@mail.ru>
2022-01-19 14:06:16 +0300

    13 UncontrolledInput + UncontrolledSelect + UncontrolledCheckbox

--- src/stories/Input.stories.tsx
+++ src/stories/Input.stories.tsx
@@ -4,8 +4,9 @@ import {action} from '@storybook/addon-actions'
 
 export default {
     title: "input",
-    //component: Input,
 }
+
+const callback = action("want to change")
 
 export const UncontrolledInput = () => <input/>
 
@@ -40,6 +41,43 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
             - actual value: {value}
         </>
     )
+}
+
+export const ControlledInput = () => {
+
+    const [parentValue, setParentValue] = useState<string>("")
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+
+    return <input value={parentValue} onChange={onChange}/>
+}
+
+export const ControlledCheckbox = () => {
+
+    const [parentValue, setParentValue] = useState<boolean>(true)
+    const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked)
+    }
+
+    return <input type={"checkbox"} checked={parentValue}  onChange={onChange}/>
+}
+
+export const ControlledSelect = () => {
+
+    const [parentValue, setParentValue] = useState<string | undefined>(undefined)
+    const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value)
+    }
+
+    return (
+        <select value={parentValue} onChange={onChange}>
+            <option>none</option>
+            <option value={"1"}>Minsk</option>
+            <option value={"2"}>Moscow</option>
+            <option value={"3"}>Kiev</option>
+        </select>
+        )
 }
 
 
